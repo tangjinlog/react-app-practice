99f73abcd5fc0cb6b5eaae081fd230107ada5393
tangjinlog <lkg70007@gmail.com>
2022-09-12 23:50:06 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import TOC from './components/TOC';
+import Content from './components/Content';
+import Subject from './components/Subject';
+import './App.css';
+
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title="CSS" desc="CSS is HyperText Markup Language."></Content>
+        <Content title="JavaScript" desc="JavaScript is best."></Content>
+      </div>
+    );
+  }
+}
+
+export default App;
